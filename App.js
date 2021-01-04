9cfc32b5a0b65fdd8eabf58c5ffdd3da8c3aeb53
Utkarsh Yadav <logn_programmer@pop-os.localdomain>
2021-01-04 16:24:49 +0530

    Completed tinder animations

--- App.js
+++ App.js
@@ -5,40 +5,49 @@ import Deck from './src/Deck';
 import { Card, Button } from 'react-native-elements';
 
 const DATA = [
-  { id: 1, text: 'Card #1', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
-  { id: 2, text: 'Card #2', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
-  { id: 3, text: 'Card #3', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
-  { id: 4, text: 'Card #4', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
-  { id: 5, text: 'Card #5', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
-  { id: 6, text: 'Card #6', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
-  { id: 7, text: 'Card #7', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
-  { id: 8, text: 'Card #8', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
-];
+  { id: 1, text: 'The wonderlust', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
+  { id: 2, text: 'The wonderlust', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
+  { id: 3, text: 'The wonderlust', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
+  { id: 4, text: 'The wonderlust', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
+  { id: 5, text: 'The wonderlust', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
+  { id: 6, text: 'The wonderlust', uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png' },
+  ];
 
 export default class App extends Component {
   renderCard(item){
     return (
       <Card
         key={item.id}
-        containerStyle={{ borderWidth: 0 }}
+        containerStyle={{ marginTop: 70, shadowRadius: 0, borderWidth: 0, borderRadius: 20, height: 400 }}
       >
         <Card.Title>{ item.text }</Card.Title>
         <Card.Image source={{ uri: item.uri }} />
-        <Text style={{ marginBottom: 10 }}> {item.text} </Text>
+        <Text style={{ marginBottom: 10, marginTop: 20 }}> {item.text} </Text>
         <Button 
           icon={{ name: 'code' }}
-          backgroundColor="#03A9F4"
+          background="#eee"
           title="View now!"
         />
       </Card>
     )
   }
+  renderNoMoreCards(){
+    return (
+        <Card containerStyle={{ marginTop: 70, shadowRadius: 0, borderWidth: 0, borderRadius: 20, height: 400 }} wrapperStyle={{ justifyContent: "center" }}>
+            <Card.Title> All done! </Card.Title>
+            <Text style={{ marginTop: 20, marginBottom: 20 }}>You're all caught up!!</Text>
+            <Button title="Get more Stuff" />
+        </Card>
+    )
+}
+
   render() {
   return (
     <View style={ styles.container }>
       <Deck 
         data={DATA}
         renderCard={this.renderCard}
+        renderNoMoreCards={this.renderNoMoreCards}
       />
       <StatusBar style="auto" />
     </View>
@@ -49,6 +58,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
   },
 });
--- src/Deck.js
+++ src/Deck.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
+import { Button } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Animated } from 'react-native';
-import { Text, View, PanResponder } from 'react-native'
+import { Text, View, PanResponder, Dimensions, LayoutAnimation, UIManager } from 'react-native';
+import { Card } from 'react-native-elements';
+
+const SCREEN_WIDTH = Dimensions.get("screen").width;
+const SWIPE_THRESHOLD = SCREEN_WIDTH*0.25;
+const SWIPE_OUT_DURATION = 250;
 
 export default class Deck extends Component {
+
+    static defaultProps = {
+        onSwipeRight: () => {},
+        onSwipeLeft: () => {},
+    }
+
     constructor(props) {
         super(props);
         const position = new Animated.ValueXY();
@@ -11,14 +24,59 @@ export default class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({ x: gesture.dx, y: gesture.dy })
             },  // executed when user starts moving the finger or dragging in the screen.
-            onPanResponderRelease: () => { }
+            onPanResponderRelease: (event, gesture) => {
+                if(gesture.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipe('right');
+                } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                    this.forceSwipe('left');
+                } else {
+                    this.resetPosition();
+                }
+            }
         });
-        this.state = { panResponder, position };
+
+        this.state = { panResponder, position, index: 0 };   // THE STATES MAIN
     }
+
+    UNSAFE_componentWillReceiveProps(nextProps){
+        if(nextProps.data !== this.props.data){
+            this.setState({ index: 0 })
+        }
+    }
+
+    UNSAFE_componentWillUpdate(){
+        UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+        LayoutAnimation.spring();
+    }
+
+    forceSwipe(direction){
+       const x =  direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(this.state.position, {
+        useNativeDriver: false,
+        toValue: { x, y: 0 },
+        duration: SWIPE_OUT_DURATION
+       }).start(() => this.onSwipeComplete(direction));
+    }
+
+   onSwipeComplete(direction){
+       const { onSwipeLeft, onSwipeRight, data } =  this.props;
+       const item = data[this.state.index];
+       direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+       this.state.position.setValue({ x: 0, y: 0 })
+       this.setState( { index: this.state.index +  1 } )
+   }
+
+    resetPosition() {
+        Animated.spring(this.state.position, {
+            toValue: { x: 0, y: 0 },
+            useNativeDriver: false
+        }).start();
+    }
+
     getCardStyle(){
         const { position } = this.state;
         const rotate = position.x.interpolate({
-            inputRange: [ -500, 0, 500 ],
+            inputRange: [ -SCREEN_WIDTH*1.5 , 0, SCREEN_WIDTH*1.5 ],
             outputRange: [ '-120deg', '0deg', '120deg' ]
         });
         return {
@@ -26,23 +84,33 @@ export default class Deck extends Component {
             transform: [{ rotate }]
         }
     }
+   
     renderCards() {
-        return this.props.data.map((item, index) => {
-            if (index === 0) {
+        if(this.state.index >= this.props.data.length){
+            return this.props.renderNoMoreCards();
+        }
+        return this.props.data.map((item, i) => {
+            if(i< this.state.index){ return null; }
+            if (i=== this.state.index) {
                 return (
                     <Animated.View
                         key={item.id}
-                        style={{ backgroundColor: "#eee", marginTop: 20 }, 
-                        this.getCardStyle()}
+                        style={[this.getCardStyle(), styles.cardStyle]}
                         {...this.state.panResponder.panHandlers}
                     >
                         {this.props.renderCard(item)}
                     </Animated.View>
                 )
             }
-            return this.props.renderCard(item);
-        })
+            return (
+                <Animated.View key={item.id} style={[styles.cardStyle, { top: 10 * ( i-this.state.index ) }]}>
+                    {this.props.renderCard(item)}
+                </Animated.View>
+            )
+        }).reverse();
     }
+
+
     render() {
         return (
             <View>         
@@ -51,3 +119,10 @@ export default class Deck extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    cardStyle: {
+        position: 'absolute',
+        width: SCREEN_WIDTH
+    }
+})
